a1f9d29eafc7cb07c2af131fd0b08d9b1c6d0678
Saravana Perumal <saravanap@infinite.com>
2026-04-05 19:59:33 +0530

    Assignment 5

---
+++ Assignment/CSharp/Assignment_5/Assignment_5/Books.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    public class Books
+    {
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public Books(string bookName, string authorName)
+        {
+            BookName = bookName;
+            AuthorName = authorName;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Book: " + BookName + ", Author: " + AuthorName);
+        }
+    }
+
+    public class BookShelf
+    {
+        private Books[] books = new Books[5]; 
+        public Books this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < books.Length)
+                    return books[index];
+                else
+                    throw new IndexOutOfRangeException("Invalid index for bookshelf");
+            }
+            set
+            {
+                if (index >= 0 && index < books.Length)
+                    books[index] = value;
+                else
+                    throw new IndexOutOfRangeException("Invalid index for bookshelf");
+            }
+        }
+    }
+
+    public class BookMain
+    {
+        static void Main(string[] args)
+        {
+            BookShelf shelf = new BookShelf();
+            for(int i = 0;i < 5; i++){
+                Console.Write("Enter the Book name : ");
+                string b_name = Console.ReadLine();
+                Console.Write("Enter the Author name : ");
+                string b_author = Console.ReadLine();
+                shelf[i] = new Books(b_name,b_author);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                shelf[i].Display();
+            }
+        }
+    }
+}
---
+++ Assignment/CSharp/Assignment_5/Assignment_5/Scholarship.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    class ScholarshipException : ApplicationException
+    {
+        public ScholarshipException(string msg) : base (msg)
+        {
+
+        }
+
+    }
+    internal class Scholarship
+    {
+        
+        public void Merit(double marks, double fees)
+        {
+            double S_amount;
+            if (marks > 90)
+            {
+                S_amount = fees / 2;
+            }
+            else if(marks > 80 && marks <= 90)
+            {
+                S_amount = (30.0 / 100.0) * fees;
+            }
+            else if( marks>=70 && marks <= 80)
+            {
+                S_amount = (20.0/100.0) * fees;
+            }
+            else
+            {
+                throw new ScholarshipException("You are not eligible for the scholarship");
+            }
+            Console.WriteLine("The scholarship amount is " + S_amount);
+        }
+        static void Main()
+        {
+            double Marks;
+            double Fees;
+            try
+            {
+                Scholarship sc = new Scholarship();
+                Console.WriteLine("Enter the Mark : ");
+                Marks = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the Fees : ");
+                Fees = Convert.ToDouble(Console.ReadLine());
+                sc.Merit(Marks, Fees);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ScholarshipException e)
+            {
+                Console.WriteLine(e .Message); 
+            }
+        }
+    }
+}
